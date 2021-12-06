74d6d341bdf0e8e177398f7fa98047c0ddace5d4
JuliaMathias <juliamss@gmail.com>
2021-12-06 14:05:33 -0300

    initial commit

---
+++ lib/aoc21.ex
@@ -0,0 +1,18 @@
+defmodule AOC21 do
+  @moduledoc """
+  Documentation for `AOC21`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AOC21.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc21_test.exs
@@ -0,0 +1,8 @@
+defmodule AOC21Test do
+  use ExUnit.Case
+  doctest AOC21
+
+  test "greets the world" do
+    assert AOC21.hello() == :world
+  end
+end
