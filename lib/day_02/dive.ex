cb6461356762ac54db4182a871041eaf3a78f970
JuliaMathias <juliamss@gmail.com>
2021-12-07 15:21:39 -0300

    feat: complete route

--- lib/day_02/dive.ex
+++ lib/day_02/dive.ex
@@ -6,15 +6,18 @@ defmodule Day02.Dive do
   # @input ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
 
   def submerge do
-    directions =
-      @input
-      |> Enum.map(fn x -> parse(x) end)
-      |> List.flatten()
+    directions = get_directions(@input)
 
     horizontal_position = sum(directions, :forward)
     depth = sum(directions, :down) - sum(directions, :up)
 
     horizontal_position * depth
+  end
+
+  def aim do
+    @input
+    |> get_directions()
+    |> do_aim(0, 0, 0)
   end
 
   defp parse(instruction) do
@@ -23,9 +26,27 @@ defmodule Day02.Dive do
     [{String.to_atom(direction), String.to_integer(distance)}]
   end
 
+  defp get_directions(input) do
+    input
+    |> Enum.map(fn x -> parse(x) end)
+    |> List.flatten()
+  end
+
   defp sum(directions, keyword) do
     directions
     |> Keyword.get_values(keyword)
     |> Enum.reduce(0, fn x, acc -> x + acc end)
+  end
+
+  defp do_aim([], horizontal_position, _aim, depth), do: horizontal_position * depth
+
+  defp do_aim([head | tail], horizontal_position, aim, depth) do
+    {direction, distance} = head
+
+    case direction do
+      :forward -> do_aim(tail, horizontal_position + distance, aim, depth + distance * aim)
+      :up -> do_aim(tail, horizontal_position, aim - distance, depth)
+      :down -> do_aim(tail, horizontal_position, aim + distance, depth)
+    end
   end
 end
